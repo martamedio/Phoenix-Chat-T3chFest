816b881b788bcfc5c7079566a8a351c3a8e7e40c
Marta Medio <marta.medio@gmail.com>
2019-03-11 16:17:11 +0100

    Build JS client code to work with the websocket/channel functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,4 +14,33 @@ import "phoenix_html"
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
+import socket from "./socket"
+
+
+// Connect and Join the chat channel
+var channel = socket.channel('my_chat:lobby', {}); 
+channel.join(); 
+
+var ul = document.getElementById('msg-list'); 
+var name = document.getElementById('name');  
+var msg = document.getElementById('msg'); 
+
+// Listen for the Enter keypress event to send a new message
+msg.addEventListener('keypress', function (event) {
+  if (event.keyCode == 13 && msg.value.length > 0) {
+    channel.push('shout', { 
+      name: name.value,     
+      message: msg.value    
+    });
+    msg.value = '';         
+  }
+});
+
+// Listen to the shout event to create a new list item for each message
+channel.on('shout', function (payload) { 
+    var li = document.createElement("li"); 
+    var name = payload.name || 'guest';    
+    li.innerHTML = '<b>' + name + '</b>: ' + payload.message; 
+    ul.appendChild(li);                   
+  });
+
